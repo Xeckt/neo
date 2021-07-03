ff0d30a54749713b5160b0573324bc3725eb5579
Node <jordansavell1198@gmail.com>
2021-07-04 00:44:44 +0100

    setup basic logging

---
+++ handler/logging/bot_log.py
@@ -0,0 +1,20 @@
+import logging
+import sys
+
+
+class BotLog:
+
+    bot_log_path = "logs/bot.log"
+    commands_log_path = "logs/commands.log"
+    system_log_path = "logs/system.log"
+    log_format = logging.Formatter("%(asctime)s | Module: %(name)s | %(levelname)s | %(message)s")
+
+    def setup_log(self, name, log_file, level=logging.INFO):
+        log_file_handler = logging.FileHandler(log_file)
+        log_file_handler.setFormatter(self.log_format)
+
+        logger = logging.getLogger(name)
+        logger.setLevel(level)
+        logger.addHandler(log_file_handler)
+        return logger
+
