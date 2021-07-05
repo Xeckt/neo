b487070fdd73759fd7036953fd6b6033ce9d2ee2
Jordan <jordansavell1198@gmail.com>
2021-07-05 11:59:25 +0100

    fix constants path

--- settings/constants.py
+++ settings/constants.py
@@ -1,4 +1,4 @@
-BOT_CONFIG_PATH = "C:\\Users\\Node\\Documents\\GitHub\\foxcord\configs\\bot\\foxcord.json"
+BOT_CONFIG_PATH = "configs/bot/foxcord.json"
 BOT_LOG_PATH = "logs/bot.log"
 COMMANDS_LOG_PATH ="logs/commands.log"
 SYSTEM_LOG_PATH="logs/system.log"
