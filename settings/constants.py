382aaea1c88445787430189d17a267f4fc2804f0
Node <jordansavell1198@gmail.com>
2021-07-06 14:44:53 +0100

    add more constants for new command system

--- settings/constants.py
+++ settings/constants.py
@@ -2,3 +2,7 @@ BOT_CONFIG_PATH = "configs/bot/foxcord.json"
 BOT_LOG_PATH = "logs/bot.log"
 COMMANDS_LOG_PATH ="logs/commands.log"
 SYSTEM_LOG_PATH="logs/system.log"
+
+COGS_USER_PATH = "handler/commands/cogs/user"
+COGS_MOD_PATH = "handler/commands/cogs/mod"
+COGS_ADMIN_PATH = "handler/commands/cogs/admin"
