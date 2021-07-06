6c372b7ac971ef92e38dae45d3214eadcb6f34b6
Node <jordansavell1198@gmail.com>
2021-07-06 23:42:58 +0100

    add loaders for admin and mod commands

--- handler/commands/controller.py
+++ handler/commands/controller.py
@@ -7,7 +7,7 @@ import os
 class FoxcordCommands:
 
     constants = settings.constants
-    commands_loaded = 0
+    amount_loaded = 0
     command_log = handler.logging.bot_log.BotLog().setup_log(__name__, constants.COMMANDS_LOG_PATH)
 
     def __init__(self, bot):
@@ -18,4 +18,18 @@ class FoxcordCommands:
         for cog in os.listdir(self.constants.COGS_USER_PATH):
             if cog.endswith(".py"):
                 self.bot.load_extension(f"{self.constants.COGS_USER_PATH.replace('/', '.') + '.' + cog[:-3]}")
-                self.commands_loaded += 1
+                self.amount_loaded += 1
+
+    def load_mod_commands(self):
+        self.command_log.info("Loading mod commands")
+        for cog in os.listdir(self.constants.COGS_MOD_PATH):
+            if cog.endswith(".py"):
+                self.bot.load_extension(f"{self.constants.COGS_MOD_PATH.replace('/', '.') + '.' + cog[:-3]}")
+                self.amount_loaded += 1
+
+    def load_admin_commands(self):
+        self.command_log.info("Loading admin commands")
+        for cog in os.listdir(self.constants.COGS_ADMIN_PATH):
+            if cog.endswith(".py"):
+                self.bot.load_extension(f"{self.constants.COGS_ADMIN_PATH.replace('/', '.') + '.' + cog[:-3]}")
+                self.amount_loaded += 1
