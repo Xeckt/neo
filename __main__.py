c65de27d22a2aa4460f09b2cc75e69d0c4b14c95
Node <jordansavell1198@gmail.com>
2021-07-06 23:44:05 +0100

    add command support in main
    add a log for commands loaded & add functions for loading mod + admin commands

--- __main__.py
+++ __main__.py
@@ -1,8 +1,8 @@
-import discord
 import handler.config.data
 import handler.logging.bot_log
 import handler.commands.controller
 import settings.constants
+import discord
 from discord.ext import commands
 
 bot_data = handler.config.data.BotData()
@@ -12,6 +12,7 @@ class Foxcord:
 
     constants = settings.constants
     bot_log = handler.logging.bot_log.BotLog().setup_log(__name__, constants.BOT_LOG_PATH)
+    command_log = handler.logging.bot_log.BotLog().setup_log("COMMANDS", constants.COMMANDS_LOG_PATH)
     foxcord = commands.Bot(command_prefix=bot_data.get_prefix())
 
     async def on_ready(self):
@@ -20,6 +21,9 @@ class Foxcord:
     async def on_message(self, ctx):
         pass
 
+    async def on_command(self, ctx):
+        self.command_log.info(f"Command invoked by {ctx.author}: {ctx.command}")
+
     def init(self):
         self.load_commands()
         self.foxcord.run(bot_data.get_token())
@@ -27,6 +31,9 @@ class Foxcord:
     def load_commands(self):
         commands = handler.commands.controller.FoxcordCommands(self.foxcord)
         commands.load_user_commands()
+        commands.load_mod_commands()
+        commands.load_admin_commands()
+        self.bot_log.info(f"Commands loaded: {commands.amount_loaded}")
 
 
 if __name__ == "__main__":
