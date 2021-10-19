167329aa183566e0211f3ea49c43bd86b21f1084
Node <jordansavell1198@gmail.com>
2021-10-19 23:15:30 +0100

    remove log for command functions

--- handler/commands/controller.py
+++ handler/commands/controller.py
@@ -32,21 +32,18 @@ class FoxcordCommands:
         self.bot = bot
 
     def load_user_commands(self):
-        self.command_log.info("Loading user commands")
         for cog in os.listdir(self.constants.COGS_USER_PATH):
             if cog.endswith(".py"):
                 self.bot.load_extension(f"{self.constants.COGS_USER_PATH.replace('/', '.') + '.' + cog[:-3]}")
                 self.commands_loaded += 1
 
     def load_mod_commands(self):
-        self.command_log.info("Loading mod commands")
         for cog in os.listdir(self.constants.COGS_MOD_PATH):
             if cog.endswith(".py"):
                 self.bot.load_extension(f"{self.constants.COGS_MOD_PATH.replace('/', '.') + '.' + cog[:-3]}")
                 self.commands_loaded += 1
 
     def load_admin_commands(self):
-        self.command_log.info("Loading admin commands")
         for cog in os.listdir(self.constants.COGS_ADMIN_PATH):
             if cog.endswith(".py"):
                 self.bot.load_extension(f"{self.constants.COGS_ADMIN_PATH.replace('/', '.') + '.' + cog[:-3]}")
