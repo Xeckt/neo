d613b1151686a9b083bac12aa2258273ae690dba
Xeckt <jordansavell1198@gmail.com>
2022-04-06 00:15:38 +0100

    improve enablemembercount

--- handler/commands/cogs/admin/enablemembercount.py
+++ handler/commands/cogs/admin/enablemembercount.py
@@ -17,22 +17,30 @@ class EnableMemberCount(commands.Cog):
         pass
 
     @commands.slash_command(
-        description="Ban a given user.",
+        description="Enable member count on a supplied voice channel via id.",
         hidden=True
     )
     @commands.has_any_role(
         data.config["adminRoleId"]
     )
     async def enablemembercount(self, interaction: disnake.ApplicationCommandInteraction, channel: disnake.VoiceChannel):
-        count = f"Member count: {interaction.guild.member_count}"
+        count_str = f"Member count: {interaction.guild.member_count}"
+        count = interaction.guild.member_count
         start_time = time.time()
         if not isinstance(channel, disnake.VoiceChannel):
             await interaction.send("You must supply a voice channel.")
             return
-        await interaction.send(f"Enabling member count on channel: {channel.name}")
+        await interaction.send(f"Enabling member count on channel: {channel.mention}")
+        prev_count = count
         while True:
-            await channel.edit(name=count)
-            await asyncio.sleep(min(120, 300) - ((time.time() - start_time) % 60.0))
+            await channel.edit(name=count_str)
+            if count == prev_count:
+                continue
+            if (count - prev_count) >= 10:
+                await interaction.send(f"{interaction.guild.name} has gained 10 more users! Old count: {prev_count} -> "+
+                                       f"New count: {count}")
+            prev_count = count
+            await asyncio.sleep(300 - ((time.time() - start_time) % 60.0))
             # Will implement lock checks for this to make sure a previously enabled async thread isn't active
 
 
