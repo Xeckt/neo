3ff6ae466eafdca810371828e81c43b31a1ff634
Jordan Savell <jordansavell1198@gmail.com>
2022-04-06 17:49:14 +0100

    Fix member count command not updating

--- handler/commands/cogs/admin/enablemembercount.py
+++ handler/commands/cogs/admin/enablemembercount.py
@@ -26,22 +26,19 @@ class EnableMemberCount(commands.Cog):
     async def enablemembercount(self, interaction: disnake.ApplicationCommandInteraction, channel: disnake.VoiceChannel):
         count_str = f"Member count: {interaction.guild.member_count}"
         count = interaction.guild.member_count
-        start_time = time.time()
         if not isinstance(channel, disnake.VoiceChannel):
             await interaction.send("You must supply a voice channel.")
             return
         await interaction.send(f"Enabling member count on channel: {channel.mention}")
         prev_count = count
         while True:
-            await channel.edit(name=count_str)
-            if count == prev_count:
-                continue
             if (count - prev_count) >= 10:
                 await interaction.send(f"{interaction.guild.name} has gained 10 more users! Old count: {prev_count} -> "+
                                        f"New count: {count}")
+            await channel.edit(name=count_str)
+            await interaction.send(f"Count updated from: {prev_count} to {count}")
             prev_count = count
-            await asyncio.sleep(300 - ((time.time() - start_time) % 60.0))
-            # Will implement lock checks for this to make sure a previously enabled async thread isn't active
+            await asyncio.sleep(3600) # 1 hour
 
 
 
