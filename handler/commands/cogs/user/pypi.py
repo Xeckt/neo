20afe02d46dbca1b0dbd016e9e6e6c2b7cd2e7cf
Xeckt <jordansavell1198@gmail.com>
2022-04-06 02:00:33 +0100

    fix pypi conditions and cleanup

--- handler/commands/cogs/user/pypi.py
+++ handler/commands/cogs/user/pypi.py
@@ -1,3 +1,5 @@
+import json
+
 import disnake
 import requests
 from disnake.ext import commands
@@ -22,31 +24,28 @@ class PyPi(commands.Cog):
     )
     @commands.cooldown(1, 5, commands.BucketType.guild)
     async def pypi(self, inter: disnake.ApplicationCommandInteraction, package):
-        print(f'https://pypi.org/pypi/{package}/json')
-        r = requests.get(f"https://pypi.org/pypi/{package}/json")
-        if r.status_code != 200:
+        url = "https://pypi.org/pypi"
+        r = requests.get(f"{url}/{package}/json")
+        if r.status_code == 200:
+            r_json = r.json()['info']
             embed = disnake.Embed(
-                title="Not found"
+                title=f"{package} v{r_json.get('version')}",
+                description=f"{r_json.get('summary')}"
             )
+            fields = {
+                'Author': 'author',
+                'Author Email': 'author_email',
+                'Discontinued': 'yanked',
+                'Keywords': 'keywords',
+                'Bugs': 'bugtrack_url',
+                'Project URL': 'project_url',
+            }
+            for name, value in fields.items():
+                if r_json.get(value):
+                    embed.add_field(name, r_json.get(value))
             await inter.send(embed=embed)
-            return
-        r = r.json()
-        embed = disnake.Embed(
-            title=f"{package} v{r.get('info').get('version')}",
-            description=f"{r.get('info').get('summary')}"
-        )
-        fields = {
-            'Author': 'author',
-            'Author Email': 'author_email',
-            'Discontinued': 'yanked',
-            'Keywords': 'keywords',
-            'Bugs': 'bugtrack_url',
-            'Project URL': 'project_url',
-        }
-        for name, value in fields.items():
-            if r.get('info').get(value):
-                embed.add_field(name, r.get('info').get(value))
-        await inter.send(embed=embed)
+        else:
+            await inter.send(f"{url}/{package}/json returned {r.status_code} reason: {r.reason}")
 
 
 def setup(bot):
