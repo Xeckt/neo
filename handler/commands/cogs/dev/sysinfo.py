69b4518b07e511529ce9cfd1ae6804975da94863
Jordan Savell <jordansavell1198@gmail.com>
2022-04-06 12:50:50 +0100

    make sysinfo command work

--- handler/commands/cogs/dev/sysinfo.py
+++ handler/commands/cogs/dev/sysinfo.py
@@ -1,6 +1,9 @@
+import csv
+import psutil
+import cpuinfo
+import disnake
 from disnake.ext import commands
 from handler.config.data import Data
-import platform
 
 
 class SysInfo(commands.Cog):
@@ -14,18 +17,37 @@ class SysInfo(commands.Cog):
         pass
 
     @commands.slash_command(
-        description="Get system information yadps-chan is running on.",
+        description="Get the server information YADPS is hosted on. Support only for Linux at the moment.",
         aliases=['si']
     )
     @commands.has_any_role(
         data.config["devRoleId"]
     )
     @commands.cooldown(1, 5, commands.BucketType.guild)
-    async def sysinfo(self, ctx):
-        await ctx.send(f"OS: {platform.system()}\n"
-                       f"OS Version: {platform.version()}\n"
-                       f"{'OS Edition: ' + platform.win32_edition() if platform.system() == 'Windows' else ''}\n")
+    async def sysinfo(self, interaction: disnake.ApplicationCommandInteraction):
+        info = {}
+        mem_v = psutil.virtual_memory()
+        mem_s = psutil.swap_memory()
+        cpu = cpuinfo.get_cpu_info()
+        with open("settings/server.info", "r") as release_file:
+            r = csv.reader(release_file, delimiter='=')
+            for k, v in r:
+                info.update({k: v})
+        embed = disnake.Embed(title="System information", description="YADPS-Chan likes beef!")
+        embed.add_field(name="Name", value=f"{info['PRETTY_NAME']}", inline=True)
+        embed.add_field(name="Architecture / ID", value=info['ID'], inline=True)
+        embed.add_field(name="Build Type", value=info['BUILD_ID'], inline=True)
+        embed.add_field(name="CPU", value=cpu['brand_raw'], inline=True)
+        embed.add_field(name="CPU Architecture", value=cpu['arch'], inline=True)
+        embed.add_field(name="CPU Hz", value=f"{cpu['hz_actual_friendly']}", inline=True)
+        embed.add_field(name="Max Memory", value=f"{self.scale_bytes_to_gb(mem_v.total):.1f}GB", inline=True)
+        embed.add_field(name="Used Memory", value=f"{self.scale_bytes_to_gb(mem_v.used):.1f}GB", inline=True)
+        embed.add_field(name="Max Swap", value=f"{self.scale_bytes_to_gb(mem_s.total):.1f}GB", inline=True)
+        embed.add_field(name="Used Swap", value=f"{self.scale_bytes_to_gb(mem_s.used):.1f}GB", inline=True)
+        await interaction.send(embed=embed)
 
+    def scale_bytes_to_gb(self, bytes):
+        return bytes / 1024 / 1024 / 1024
 
 def setup(bot):
     bot.add_cog(SysInfo(bot))
