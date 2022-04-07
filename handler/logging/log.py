b275925f3652eed75ddf301150f5123442fc3f40
Jordan Savell <jordansavell1198@gmail.com>
2022-04-07 17:19:06 +0100

    change logging times

--- handler/logging/log.py
+++ handler/logging/log.py
@@ -7,7 +7,7 @@ class Log:
     log_format = logging.Formatter("%(asctime)s | Module: %(name)s | %(levelname)s | %(message)s")
 
     def create(self, name, log_file, level=logging.INFO):
-        log_file_handler = logging.FileHandler(log_file + time.strftime("_%Y-%m-%d-%H"))
+        log_file_handler = logging.FileHandler(log_file + time.strftime("-%Y-%m-%d"))
         log_file_handler.setFormatter(self.log_format)
         console_handler = logging.StreamHandler()
         console_handler.setFormatter(self.log_format)
