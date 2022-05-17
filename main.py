e7f25f17660582f444b3a64986c3cbcef1deebfc
Xeckt <jordansavell1198@gmail.com>
2022-05-17 10:42:52 +0100

    add test guild from config id

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import sys
+from neo.config.data import Data
 from neo.bot.neo import Neo
+
 
 if __name__ == "__main__":
     if sys.version_info[0:2] != (3, 10):
         print(f"Python version must be minimum 3.10. Currently detected version: "
               f"{str(sys.version_info.major) + '.' + str(sys.version_info.minor)}")
         exit(1)
-    neo = Neo(test_guilds=[])
+    data = Data()
+    neo = Neo(test_guilds=[data.guildId])
     neo.run(neo.data.token)
 
