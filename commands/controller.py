742d3fd93b7edda7c73584ae31da6528dc16c65d
xeckt <jordansavell1198@gmail.com>
2025-02-07 23:53:12 +0000

    remove unnecessary config fields, improve command controller

--- commands/controller.py
+++ commands/controller.py
@@ -5,43 +5,54 @@ from logger.log import Log
 class CommandController:
     data = NeoConfig()
     command_log = Log().create(__name__, data.commandLog)
+
+    cog_path = "commands"
     total_loaded = 0
 
-    _state_load = "load"
-    _state_reload = "reload"
-    _state_unload = "unload"
+    STATE_LOAD = "load"
+    STATE_RELOAD = "reload"
+    STATE_UNLOAD = "unload"
 
     def __init__(self, bot):
         self.bot = bot
 
     def load_cmds(self):
-        if self.data.enableDevCommands:
-            self.set_cog_state(self.data.devCog, self._state_load)
-            if self.data.enableCommandDebug:
-                self.command_log.info("Developer commands loaded")
-        if self.data.enableAdminCommands:
-            self.set_cog_state(self.data.adminCog, self._state_load)
-            if self.data.enableCommandDebug:
-                self.command_log.info("Admin commands loaded")
-        if self.data.enableModCommands:
-            self.set_cog_state(self.data.modCog, self._state_load)
-            if self.data.enableCommandDebug:
-                self.command_log.info("Moderator commands loaded")
-        if self.data.enableUserCommands:
-            self.set_cog_state(self.data.userCog, self._state_load)
-            if self.data.enableCommandDebug:
-                self.command_log.info("User commands loaded")
+        self.command_log.info("Loading command cogs")
+        self.set_cog_state("dev", self.STATE_LOAD)
+        self.set_cog_state("admin", self.STATE_LOAD)
+        self.set_cog_state("mod", self.STATE_LOAD)
+        self.set_cog_state("user", self.STATE_LOAD)
         self.command_log.info(f"Total commands loaded: {self.total_loaded}")
 
-    def set_cog_state(self, module, state):
-        path_string = self.data.cogPath + '.' + module
+    def set_cog_state(self, cog, state):
+        path_string = self.cog_path + '.' + cog
+
         for cmd in os.listdir(path_string.replace('.', os.path.sep)):
+
             if cmd.endswith('.py') and cmd != "__init__.py":
+
                 getattr(self.bot, "%s_extension" % state)(*{path_string + f".{cmd[:-3]}"})
-                self.total_loaded += 1
+                self.command_log.debug(f"Set %s command %s state to %s", cog, cmd[:-3], state)
+
+                if state == self.STATE_LOAD:
+                    self.total_loaded += 1
+
+                elif state == self.STATE_UNLOAD:
+                    self.total_loaded -= 1
 
     def set_command_state(self, cog, state, command: str):
-        cog = self.data.cogPath + '.' + cog
+        cog = self.cog_path + '.' + cog
+
         for cmd in os.listdir(cog.replace('.', os.path.sep)):
+
             if cmd != "__init__.py" and cmd == command:
+
                 getattr(self.bot, "%s_extension" % state)(*{cog + f".{command[:-3]}"})
+
+                self.command_log.debug(f"Set %s command %s state to %s", cog, command[:-3], state)
+
+                if state == self.STATE_LOAD:
+                    self.total_loaded += 1
+
+                elif state == self.STATE_UNLOAD:
+                    self.total_loaded -= 1
