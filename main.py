b140836aaf839e38e9960ed91ee37f0330f2a88b
xeckt <jordansavell1198@gmail.com>
2025-02-07 22:23:55 +0000

    fix main.py version checking and imports

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import sys
-from neo.config.data import Data
-from neo.bot.neo import Neo
-
+from config.neoconfig import NeoConfig
+from bot.neo import Neo
 
 if __name__ == "__main__":
-    if sys.version_info[0:2] != (3, 10):
+
+    if sys.version_info.major < 3 and sys.version_info.minor < 10:
         print(f"Python version must be minimum 3.10. Currently detected version: "
               f"{str(sys.version_info.major) + '.' + str(sys.version_info.minor)}")
         exit(1)
-    data = Data()
+
+    data = NeoConfig()
     neo = Neo(test_guilds=[data.guildId])
     neo.run(neo.data.token)
-
