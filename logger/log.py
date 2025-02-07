eafbed70df1b056957e29949a4595dd36ee84495
xeckt <jordansavell1198@gmail.com>
2025-02-07 23:52:30 +0000

    add coloured logging for console

--- logger/log.py
+++ logger/log.py
@@ -2,16 +2,51 @@ import logging
 from logging.handlers import TimedRotatingFileHandler
 class Log:
 
-    formatter = logging.Formatter("%(asctime)s | Module: %(name)s | %(levelname)s | %(message)s")
+    formatter = "%(asctime)s | Module: %(name)s | %(levelname)s | %(message)s"
 
-    def create(self, name, log_file, level=logging.INFO):
+    def create(self, name, log_file, level=logging.DEBUG):
+        # handler for file output
         rotate_handler = TimedRotatingFileHandler(filename=log_file, when='h', interval=1, backupCount=2)
-        rotate_handler.setFormatter(self.formatter)
+        rotate_handler.setFormatter(logging.Formatter(self.formatter))
+
+        # handler for console output
         console_handler = logging.StreamHandler()
-        console_handler.setFormatter(self.formatter)
+        console_handler.setFormatter(CustomFormatter(self.formatter))
 
         logger = logging.getLogger(name)
         logger.setLevel(level)
+
         logger.addHandler(rotate_handler)
         logger.addHandler(console_handler)
+
         return logger
+
+class CustomFormatter(logging.Formatter):
+    """
+    Logging colored formatter
+    https://alexandra-zaharia.github.io/posts/make-your-own-custom-color-formatter-with-python-logging/
+    """
+
+    grey = '\x1b[38;21m'
+    orange = '\x1b[38;5;208m'
+    blue = '\x1b[38;5;39m'
+    yellow = '\x1b[38;5;226m'
+    red = '\x1b[38;5;196m'
+    bold_red = '\x1b[31;1m'
+    reset = '\x1b[0m'
+
+    def __init__(self, fmt):
+        super().__init__()
+        self.fmt = fmt
+        self.FORMATS = {
+            logging.DEBUG: self.orange + self.fmt + self.reset,
+            logging.INFO: self.blue + self.fmt + self.reset,
+            logging.WARNING: self.yellow + self.fmt + self.reset,
+            logging.ERROR: self.red + self.fmt + self.reset,
+            logging.CRITICAL: self.bold_red + self.fmt + self.reset
+        }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
